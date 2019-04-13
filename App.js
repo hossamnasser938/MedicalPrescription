178a2e418ece917a9e9917a5292d1dc1f80e651d
Hosam <hossamnasser938@gmail.com>
2019-04-13 19:36:21 +0200

    Test login with custom permissions _ works

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
 import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Button} from 'react-native';
 import LoginComponent from './src/components/Login';
+import { LoginManager, AccessToken } from "react-native-fbsdk";
 
 export default class App extends Component {
+  customLoginHandler = () => {
+    LoginManager.logInWithReadPermissions(["public_profile"]).then(
+      function(result) {
+        console.log( "result: ", result );
+        if (result.isCancelled) {
+          console.log("Login cancelled");
+        } else {
+          console.log(
+            "Login success with permissions: " +
+              result.grantedPermissions.toString()
+          );
+
+          AccessToken.getCurrentAccessToken().then(
+            (data) => {
+              console.log(data.accessToken.toString())
+            }
+          )
+        }
+      },
+      function(error) {
+        console.log("Login fail with error: " + error);
+      }
+    );
+  }
+  
+  testHandler = () => {
+    console.log( "test" );
+  };
+  
   render() {
     return (
       <View style={styles.container}>
-        <LoginComponent />
+        <Button
+          style = { styles.btn }
+          title = "Login using Facebook"
+          onPress = { this.customLoginHandler }
+        />
+
+        <Button
+          style = { styles.btn }
+          title = "test"
+          onPress = { this.testHandler }
+        />
       </View>
     );
   }
@@ -18,5 +58,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  btn: {
+    margin: 16
   }
 });
